b1481ab8f85147d146f356b0625155bb648e5d78
Emil Dimitrov <emil_132@abv.bg>
2017-10-11 14:50:20 +0300

    Added CommonNews.Data.Common

---
+++ src/Data/CommonNews.Data.Common/Contracts/IEfRepository{T}.cs
@@ -0,0 +1,22 @@
+﻿namespace CommonNews.Data.Common.Contracts
+{
+    using System.Linq;
+
+    using Models.Contracts;
+
+    public interface IEfRepository<T>
+        where T : class, IAuditInfo, IDeletableEntity
+    {
+        IQueryable<T> All();
+
+        IQueryable<T> AllWithDeleted();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ src/Data/CommonNews.Data.Common/SaveContext.cs
@@ -0,0 +1,19 @@
+﻿namespace CommonNews.Data.Common
+{
+    using CommonNews.Data.Common.Contracts;
+
+    public class SaveContext : ISaveContext
+    {
+        private readonly MsSqlDbContext context;
+
+        public SaveContext(MsSqlDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Commit()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
