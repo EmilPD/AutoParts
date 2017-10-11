5cbe8cc124fcaccc6436ee99f34020f1eb5afcf2
Emil Dimitrov <emil_132@abv.bg>
2017-10-11 12:36:20 +0300

    Extracted ApplicationUser to Data.Models

---
+++ src/Data/CommonNews.Data.Models/ApplicationUser.cs
@@ -0,0 +1,56 @@
+﻿namespace CommonNews.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Contracts;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class ApplicationUser : IdentityUser, IDeletableEntity, IAuditInfo
+    {
+        private ICollection<Post> posts;
+        private ICollection<Comment> comments;
+
+        public ApplicationUser()
+        {
+            this.posts = new HashSet<Post>();
+            this.comments = new HashSet<Comment>();
+            this.CreatedOn = DateTime.Now;
+        }
+
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public virtual ICollection<Post> Posts
+        {
+            get { return this.posts; }
+            set { this.posts = value; }
+        }
+
+        public virtual ICollection<Comment> Comments
+        {
+            get { return this.comments; }
+            set { this.comments = value; }
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
