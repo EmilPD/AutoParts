e6a41b2454d104f83855ed53a1de60ddc342271c
Emil Dimitrov <emil_132@abv.bg>
2017-10-11 15:10:06 +0300

    Added Ninject

---
+++ src/Web/CommonNews.Web/App_Start/ApplicationSignInManager.cs
@@ -0,0 +1,24 @@
+﻿namespace CommonNews.Web
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security;
+
+    using CommonNews.Data.Models;
+
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)this.UserManager);
+        }
+    }
+}
---
+++ src/Web/CommonNews.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿namespace CommonNews.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using Services.Data;
+    using ViewModels.Home;
+
+    public class HomeController : BaseController
+    {
+        private readonly IPostsService postsService;
+
+        public HomeController(IPostsService postsService)
+        {
+            this.postsService = postsService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var posts = this.postsService
+                .GetAll()
+                .ToList()
+                .Select(x => this.Mapper.Map<PostViewModel>(x))
+                .ToList();
+
+            var viewModel = new HomeViewModel()
+            {
+                Posts = posts
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
