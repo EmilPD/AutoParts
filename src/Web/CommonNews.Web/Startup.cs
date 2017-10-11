99bd9a58181cda2b85003b188a3af66a6161120c
Emil Dimitrov <emil_132@abv.bg>
2017-10-11 08:46:19 +0300

    Initial

---
+++ src/Web/CommonNews.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommonNews.Web.Startup))]
+namespace CommonNews.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
