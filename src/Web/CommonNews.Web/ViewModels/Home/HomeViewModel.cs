8152d1ad8234a6808fb93df8c1f764f665e0de1b
Emil Dimitrov <emil_132@abv.bg>
2017-10-11 15:03:05 +0300

    Extracted ViewModels in own files

---
+++ src/Web/CommonNews.Web/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonNews.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    public class HomeViewModel
+    {
+        public ICollection<PostViewModel> Posts { get; set; }
+    }
+}
