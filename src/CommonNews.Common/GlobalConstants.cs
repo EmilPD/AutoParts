74452e61271a57ec438a29a72f7690a2821802ba
Emil Dimitrov <emil_132@abv.bg>
2017-10-11 09:43:42 +0300

    Added CommonNews.Common

---
+++ src/CommonNews.Common/GlobalConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonNews.Common
+{
+    public class GlobalConstants
+    {
+        public const string AdministratorRoleName = "Admin";
+        public const int PasswordMinLength = 1;
+    }
+}
