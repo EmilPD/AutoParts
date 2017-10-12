6c65dd8313ea78b481292493ea07435dffc71560
Emil Dimitrov <emil_132@abv.bg>
2017-10-12 09:43:26 +0300

    Added PostFormViewModel

---
+++ src/Web/CommonNews.Web/ViewModels/Post/PostFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CommonNews.Web.ViewModels.Post
+{
+    using System.Collections.Generic;
+    using Data.Models;
+
+    public class PostFormViewModel
+    {
+        public IEnumerable<PostCategory> Categories { get; set; }
+
+        public Post Post { get; set; }
+    }
+}
