7131717d0e5ffdc5f13df4dcd4fd997f89244729
Emil Dimitrov <emil_132@abv.bg>
2017-10-13 17:05:33 +0300

    AppHarbor db setup

---
+++ src/Data/CommonNews.Data.Common/Configuration.cs
@@ -0,0 +1,19 @@
+﻿namespace CommonNews.Data.Common
+{
+    using System.Data.Entity.Migrations;
+    using Bytes2you.Validation;
+
+    public class Configuration : DbMigrationsConfiguration<MsSqlDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(MsSqlDbContext context)
+        {
+            Guard.WhenArgument(context, "MsSqlDbContext").IsNull().Throw();
+        }
+    }
+}
--- src/Data/CommonNews.Data/Migrations/Configuration.cs
+++ src/Data/CommonNews.Data/Migrations/Configuration.cs
@@ -1,20 +1,19 @@
-﻿namespace CommonNews.Data.Migrations
-{
-    using System.Data.Entity.Migrations;
-    using Bytes2you.Validation;
-    using Common;
+﻿//namespace CommonNews.Data.Migrations
+//{
+//    using System.Data.Entity.Migrations;
+//    using Bytes2you.Validation;
 
-    public sealed class Configuration : DbMigrationsConfiguration<MsSqlDbContext>
-    {
-        public Configuration()
-        {
-            this.AutomaticMigrationsEnabled = false;
-            this.AutomaticMigrationDataLossAllowed = false;
-        }
+//    public class Configuration : DbMigrationsConfiguration<MsSqlDbContext>
+//    {
+//        public Configuration()
+//        {
+//            this.AutomaticMigrationsEnabled = false;
+//            this.AutomaticMigrationDataLossAllowed = false;
+//        }
 
-        protected override void Seed(MsSqlDbContext context)
-        {
-            Guard.WhenArgument(context, "MsSqlDbContext").IsNull().Throw();
-        }
-    }
-}
+//        protected override void Seed(MsSqlDbContext context)
+//        {
+//            Guard.WhenArgument(context, "MsSqlDbContext").IsNull().Throw();
+//        }
+//    }
+//}
