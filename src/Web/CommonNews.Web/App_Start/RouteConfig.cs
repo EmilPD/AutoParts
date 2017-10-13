91b70a35db2d60a152eed623120d48d8ddb4ba2f
Emil Dimitrov <emil_132@abv.bg>
2017-10-13 12:48:14 +0300

    Added custom route for Categories

--- src/Web/CommonNews.Web/App_Start/RouteConfig.cs
+++ src/Web/CommonNews.Web/App_Start/RouteConfig.cs
@@ -11,6 +11,11 @@
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Categories",
+                url: "categories/{categoryName}",
+                defaults: new { controller = "Categories", action = "Index" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
